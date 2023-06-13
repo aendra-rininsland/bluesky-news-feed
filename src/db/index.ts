659de51ef1dde3be370e788a0c4dcee1738982b6
Ændra Rininsland <hello@aendra.com>
2023-06-13 18:04:39 +0100

    Adding pg support

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,27 @@
 import SqliteDb from 'better-sqlite3'
-import { Kysely, Migrator, SqliteDialect } from 'kysely'
+import { Kysely, Migrator, SqliteDialect, PostgresDialect } from 'kysely'
 import { DatabaseSchema } from './schema'
 import { migrationProvider } from './migrations'
+import { Pool } from 'pg'
+
+const dialect = process.env.DATABASE_URL ? 'pg' : 'sqlite'
 
 export const createDb = (location: string): Database => {
+  if (dialect === 'pg') {
+    const params = new URL(process.env.DATABASE_URL || '')
+
+    return new Kysely<DatabaseSchema>({
+      dialect: new PostgresDialect({
+        pool: new Pool({
+          user: params.username,
+          password: params.password,
+          host: params.hostname,
+          database: params.pathname.split('/')[1],
+          ssl: { rejectUnauthorized: false },
+        }),
+      }),
+    })
+  }
   return new Kysely<DatabaseSchema>({
     dialect: new SqliteDialect({
       database: new SqliteDb(location),
