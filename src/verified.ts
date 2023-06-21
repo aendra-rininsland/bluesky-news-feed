65bc651c96dd76c678ee99db48797840b69fa365
Ændra Rininsland <hello@aendra.com>
2023-06-21 12:57:00 +0100

    Fixing verified

--- src/verified.ts
+++ src/verified.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs'
 
 export const VERIFIED_DIDS = readFileSync('./VERIFIED.txt', 'utf8')
-  .replace(/\#.*$/, '')
   .split('\n')
-  .map((line) => line.trim())
+  .map((line) => line.replace(/\s?\#.+$/, '').trim())
+  .filter((i) => i)
